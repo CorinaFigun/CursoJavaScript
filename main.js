a203b970fe59cd692b1e384db327d12dbbdedde6
Corina Figun <corifigun@gmail.com>
2023-01-17 18:19:51 -0300

    funcion con switch

--- main.js
+++ main.js
@@ -1,82 +1,68 @@
-function QueRico () {
-  
-  
-const pastas = ["pastas", "pasta", "tallarines", "ravioles", "ñoquis", "fideos", "fideo"]
+function queRico() {
 
-const pizza = ["pizza", "piza", "pisa", "pizzeta", "muzzarella", "fugazzeta"]
+  let recetasEspecificas = prompt("¿Qué tipo de recetas te gusta hacer? 1)Pastas 2)Pizza 3)Postres 0)Para salir ")
+  while (recetasEspecificas != 0) {
 
-const postres = ["postre", "postres", "tiramisu", "mousse"]
+    switch (recetasEspecificas) {
 
+      case "1":
+        alert("te dejamos el link de nuestras mejores pastas:");
 
-if (pastas) { prompt ("te dejamos el link de nuestras mejores pastas:")}
+      case "2":
+        alert("te dejamos el link de nuestras mejores pizzas:");
 
-if (pizza) { prompt ("te dejamos el link de nuestras mejores pizzas")}
+      case "3":
+        alert("te dejamos el link de nuestras mejores postres:");
+default: 
+alert ("Que recetas te gustan?")
 
-if (postres) {prompt ("te dejamos el link de nuestros mejores postres")}
-
-else {prompt ("lo siento")}
+        recetasEspecificas = prompt("¿Qué tipo de recetas te gusta hacer? 1)Pastas 2)Pizza 3)Postres 0)Para salir ")
+    }
+  }
 }
 
-let nombreIngresado = prompt ("Ingrese su nombre")
 
-console.log ("Mensaje de prueba")
+let nombreIngresado = prompt("Ingrese su nombre")
+
+console.log("Mensaje de prueba")
 let nombre = "Corina Figun"
-console.log (nombre)
+console.log(nombre)
 
-alert ("Hola" + " " + nombreIngresado);
+alert("Hola" + " " + nombreIngresado);
 
-let edad = parseInt (prompt ("Contanos cuál es tu edad"));
+let edad = parseInt(prompt("Contanos cuál es tu edad"));
 
 do {
   if (edad >= 18) {
     console.log("Eres mayor de edad");
-    alert ("Eres mayor de edad,  puedes hacer nuestras recetas sobre tragos");
-    
+    alert("Eres mayor de edad,  puedes hacer nuestras recetas sobre tragos");
+
   } else {
     console.log("Eres menor de edad");
-    alert ("Eres menor de edad, no puedes hacer nuestras recetas sobre tragos")
+    alert("Eres menor de edad, no puedes hacer nuestras recetas sobre tragos")
   }
-} while(isNaN(edad))
+} while (isNaN(edad))
 
 
 
 let cocinar = "TeGustaCocinar"
 
 do {
-alert= ("Ahora queremos saber si nuestro contenido es el indicado para vos")
-cocinar = prompt ("contanos si te gusta cocinar para conocer el contenido de nuestro sitio (contestar con si o no)")
+  alert = ("Ahora queremos saber si nuestro contenido es el indicado para vos")
+  cocinar = prompt("contanos si te gusta cocinar para conocer el contenido de nuestro sitio (contestar con si o no)")
 }
 
-while (cocinar != "si" )   {
+while (cocinar != "si") {
 
-alert= ("si tu respuesta no es afirmativa no tenemos cómo ayudarte")
-Cocinar = prompt ("entonces estas en el lugar indicado")}
-
-let gustosCocina = prompt ("¿Qué tipo de recetas te gusta hacer?")
-  
-console.log ("gustos cocina")
-
-
-  
-  
-const pastas = ["pastas", "pasta", "tallarines", "ravioles", "ñoquis", "fideos", "fideo"]
-  
-const pizza = ["pizza", "piza", "pisa", "pizzeta", "muzzarella", "fugazzeta"]
-  
-const postres = ["postre", "postres", "tiramisu", "mousse"]
-  
-let recetas = "RecetasEspecificas"
-
-do {
-alert= ("¿Qué tipo de recetas te gusta hacer?")
-pastas = prompt ("te dejamos el link de nuestras mejores pastas:")
+  alert = ("si tu respuesta no es afirmativa no tenemos cómo ayudarte")
+  Cocinar = prompt("entonces estas en el lugar indicado")
 }
 
-while (recetas != "pastas" )  {
 
-  alert= ("si tu respuesta no es afirmativa no tenemos cómo ayudarte")
-  Cocinar = prompt ("entonces estas en el lugar indicado")
 
-}
-QueRico ()
+
+
+
+
+queRico()
 
