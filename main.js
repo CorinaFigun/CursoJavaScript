f70a45b9ab2d991cd08e1ace8ec1f182f79641a7
Corina Figun <corifigun@gmail.com>
2023-01-30 19:03:31 -0300

    nueva funcion

--- main.js
+++ main.js
@@ -18,16 +18,16 @@ function queRico() {
     switch (recetas) {
 
       case "1":
-        alert ("te dejamos el link de nuestras mejores pastas: www.RecetasCori/pastas.net");
+        alert("te dejamos el link de nuestras mejores pastas: www.RecetasCori/pastas.net");
         break;
       case "2":
-        alert ("te dejamos el link de nuestras mejores pizzas: www.RecetasCori/pizzas.net");
+        alert("te dejamos el link de nuestras mejores pizzas: www.RecetasCori/pizzas.net");
         break;
       case "3":
-        alert ("te dejamos el link de nuestras mejores postres: www.RecetasCori/postres.net");
+        alert("te dejamos el link de nuestras mejores postres: www.RecetasCori/postres.net");
         break;
       default:
-        alert ("seguramente encontraras otras recetas de tu interes");
+        alert("seguramente encontraras otras recetas de tu interes");
         break;
     }
 
@@ -56,183 +56,151 @@ do {
 
 let cocinar = "TeGustaCocinar"
 
-alert ("Ahora queremos saber si nuestro contenido es el indicado para vos")
+alert("Ahora queremos saber si nuestro contenido es el indicado para vos")
 
 do {
   cocinar = prompt("contanos si te gusta cocinar para conocer el contenido de nuestro sitio (contestar con si o no)");
 
 } while (cocinar != "si");
 
-  alert ("entonces estas en el lugar indicado")
+alert("entonces estas en el lugar indicado")
 
-
-alert ("si tu respuesta no es afirmativa no tenemos cómo ayudarte");
-
+while (cocinar == "no");
+alert("nopodemos auyudarte")
 
 
 queRico();
 
-function KilogramosAOtro (valor1) {
+function KilogramosAOtro(valor1) {
   return (cantidad1) => (valor1 * cantidad1);
-  }
-  
-  const KilogramosALibras = KilogramosAOtro (2.20462);
-  const KilogramosAOnzas= KilogramosAOtro (35.274);
-  const KilogramosAGramos= KilogramosAOtro (1000);
+}
 
- 
+const KilogramosALibras = KilogramosAOtro(2.20462);
+const KilogramosAOnzas = KilogramosAOtro(35.274);
+const KilogramosAGramos = KilogramosAOtro(1000);
 
-  const cantidad1 = 2; 
-  console.log (KilogramosAGramos (cantidad1)); 
-  
-  alert (KilogramosAGramos (cantidad1))
-  
-  function LibrasAOtro (valor2) {
-    return (cantidad2) => (valor2 / cantidad2);
-     }
-    
-     const LibrasAKilogramos = LibrasAOtro (2.2046);
-    const LibrasAGramos = LibrasAOtro (0.0022046);
-    const LibrasAOnzas = LibrasAOtro (0.0625);
-    
-    const cantidad2 = 4;
-    
-    console.log (LibrasAKilogramos (cantidad2)); 
-     alert (LibrasAKilogramos (cantidad2));
-    
-    
-    
-    
-    
-    
-    function OnzasAOtro (valor3) {
-    return (cantidad3) => (valor3 / cantidad3);
-     }
-    
-    
-    const OnzasAKilogramos = OnzasAOtro (35.274);
-    const OnzasAGramos = OnzasAOtro (0.035274);
-    const OnzasALibras = OnzasAOtro (16);
-    
-    const cantidad3 = 3;
-    
-    console.log (OnzasALibras (cantidad3)); 
-    alert (OnzasALibras (cantidad3));
-    
-  
-  
-  
-  
-  
+
+
+const cantidad1 = 2;
+console.log(KilogramosAGramos(cantidad1));
+
+alert(KilogramosAGramos(cantidad1))
+
+function LibrasAOtro(valor2) {
+  return (cantidad2) => (valor2 / cantidad2);
+}
+
+const LibrasAKilogramos = LibrasAOtro(2.2046);
+const LibrasAGramos = LibrasAOtro(0.0022046);
+const LibrasAOnzas = LibrasAOtro(0.0625);
+
+const cantidad2 = 4;
+
+console.log(LibrasAKilogramos(cantidad2));
+alert(LibrasAKilogramos(cantidad2));
 
 
 
 
-function TiposRecetas (tipo, tiempo, dificultad, link ) {
 
-  this.tipo = tipo;
-  this.tiempo = tiempo;
-  this.dificultad = dificultad;
-  this.link = link;
-  
-  this.mostrarDificultad = function () {
-    console.log("Esta receta tiene un grado de dificultad" + " " + dificultad)
-this.mostrarDificultad = alert ("Esta receta tiene un grado de dificultad" + " " + dificultad)
-  }
+
+function OnzasAOtro(valor3) {
+  return (cantidad3) => (valor3 / cantidad3);
 }
 
 
+const OnzasAKilogramos = OnzasAOtro(35.274);
+const OnzasAGramos = OnzasAOtro(0.035274);
+const OnzasALibras = OnzasAOtro(16);
 
-const receta1 = new TiposRecetas ("pastas","treinta minutos","baja", "www.recetasCori/pennerigatti.net"); 
-  
-const receta2 = new TiposRecetas ("postres", "una hora y media" , "alta", "www.recetascori/rogel.net");
+const cantidad3 = 3;
 
-const receta3 = new TiposRecetas ("pizzas", "cuarenta minutos", "baja", "www.recetascori/pizzaroqueydurazno"); 
-  
-const receta4 = new TiposRecetas ("postres", "cuarenta minutos", "baja", "www.recetascori/moussemaracuya"); 
-
-  receta3.mostrarDificultad();
-
-if ("link" in receta1) {
-
-  console.log("Tenemos el link de la receta de penne rigatti")
-
-}
-alert ("Tenemos el link de la receta de penne rigatti")
-
-for (const propiedad in receta1) {
-console.log (receta1 [propiedad]);
-}
+console.log(OnzasALibras(cantidad3));
+alert(OnzasALibras(cantidad3));
 
 
 
-const ingredientes = [ "harina", "aceite", "huevos", "polvo de hornear", "levadura", "verduras", "frutas", "azucar", "sal", "leche", "agua" ] 
-for (const ingrediente of ingredientes) {
-  console.log (ingrediente); 
-  alert (ingrediente)
-}
 
 
 
-class Postres {
-
-  constructor (harina, huevos, azucar, fruta, leche, crema, otros) {
-  this.harina = harina;
-  this.huevos= huevos;
-  this.azucar = azucar;
-  this.fruta= fruta;
-  this.leche= leche;
-  this.crema = crema;
-  this.otros= otros;
-}
-
-mostrarIngredientesSinFrutas (ingredientesRecetaSinFrutas) {
-return ingredientesRecetaSinFrutas - this.fruta; 
-}
-
-mostrarIngredientesSinTACC (ingredientesSinTACC) {
-  return ingredientes - this.harina;
-}
-
-mostrarIngredientesSinLactosa (IngredientesSinLactosa) {
-  return IngredientesSinLactosa - this.leche - this.crema;
-}
-}
 
 
-const postres = [
-  new Postres ( "no" ,  "no",  "100 gr",  "1 maracuya",  "no",  "quinientos gr", "leche condensada - 25 ml" ),
-  new Postres ("500 gr",  "2",  "100 gr",  "no",  "no",  "no", "no"),
-];
 
-let operacion = prompt ("Qué te gustaría? 1- postres con frutas 2- postres faciles 3- tortas 4-no me gustan los postres" );
-while (operacion !== "4") {
-  switch (operacion) {
 
-    case "1":
-    postresConFrutas () 
-      operacion = prompt ("Queres hacer una receta con frutas?")
-    
-    break;
+class TiposRecetas {
+  constructor(tiempo, dificultad, link) {
 
-    case "2":
-postresFaciles ()
-operacion = prompt ("Queres hacer una receta fácil?")
-    break;
+    this.tiempo = tiempo;
+    this.dificultad = dificultad;
+    this.link = link;
 
-    case "3":
-tortas ()
-operacion = prompt ("Queres hacer una torta?")
-    break;
-  
 
-    default:
-      console.log ("opcion incorrecta");
-      break;
+    this.mostrarDificultad = function () {
+      console.log("Esta receta tiene un grado de dificultad" + " " + dificultad)
+      this.mostrarDificultad = alert("Esta receta tiene un grado de dificultad" + " " + dificultad)
+    }
   }
 }
 
 
 
-const input = getElementById ("input1");
-console.log (input.value);
+const receta1 = new TiposRecetas("treinta minutos", "baja", "www.recetasCori/pennerigatti.net");
+
+const receta2 = new TiposRecetas("una hora y media", "alta", "www.recetascori/rogel.net");
+
+const receta3 = new TiposRecetas("cuarenta minutos", "baja", "www.recetascori/pizzaroqueydurazno");
+
+const receta4 = new TiposRecetas("cuarenta minutos", "media", "www.recetascori/moussemaracuya");
+
+receta3.mostrarDificultad();
+
+const recetasTipos = [receta1, receta2, receta3, receta4]
+let dificultades = []
+
+function elegirDificultad() {
+  let recetaElegidaDificultad;
+  do {
+    recetaElegidaDificultad = prompt("Ingrese cuanta dificultad desea que tenga la próxima receta que va a cocinar: 1-baja 2-media 3-alta. Para salir, ingrese 0. Para buscar recetas según ingredientes presione 4");
+
+    if (recetaElegidaDificultad >= 0 && recetaElegidaDificultad <= 4) {
+      switch (recetaElegidaDificultad) {
+        case 0:
+          alert("Saliste, nos vemos en la próxima receta!")
+          break;
+
+        case 4:
+          recetasPorIngredientes()
+          break;
+
+        default:
+          buscarRecetaDificultad (recetaElegidaDificultad)
+          break;
+      }
+    }
+    else {
+      alert("ingreso un valor inválido")
+    }
+  }
+
+ while (recetaElegidaDificultad !== 4 && recetaElegidaDificultad !== 0)
+}
+
+function buscarRecetaDificultad (valor) {
+const recetaBuscadaDificultad = recetasTipos.find (recetaDificultad => receta.id === valor)
+elegirDificultad (recetaBuscadaDificultad)
+}
+
+function elegirDificultad (recetaElegidaDificultad) {
+  let dificultades = prompt ("Ingrese dificultad deseada");
+if (dificultades.some (recetaDificultad => recetaDificultad.id  === recetaElegidaDificultad.id)) {
+  dificultades.forEach (recetaDificultad=> {
+    if (recetaDificultad.id === recetaElegidaDificultad.id) {
+      recetaDificultad.dificultades = recetaElegidaDificultad.dificultades + dificultades 
+    }
+  } )
+} else {
+  alert ("Muchas gracias")
+}
+}
+
+ElegirReceta ();
