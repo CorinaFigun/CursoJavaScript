d2d2f7f602c7ee04d7a565c2ff67f2778175240d
Corina Figun <corifigun@gmail.com>
2023-02-18 19:02:09 -0300

    armando carrito

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
 
+//Funciones
+
+function renderizarProductos (productos) {
+listaProductos.innerHTML= "";
+for (const producto of productos) {
+  const div = document.createElement ("div");
+div.className ="producto";
+  const h2 = document.createElement ("h2");
+  h2.innerHTML = `${producto.nombre}`;
 
 
+  const h4 = document.createElement ("h4");
+  h2.innerHTML = `${producto.precio}`;
+
+
+  const button = document.createElement ("button");
+  button.innerHTML = "Agregar al carrito";
+
+  button.addEventListener ("click", () => {});
+  console.log (producto);
+
+ div.append (h2, h4, button);
+ listaProductos.append (div);
+}
+}
 
 function calcularEdad(nombre, diaNacimiento, mesNacimiento, anioNacimiento) {
   var nombre = document.getElementById("nombre").value;
@@ -220,9 +243,15 @@ const Postres = [
 swal (Postres)
   }
 
+
+
+  const listaProductos = document.getElementById (listaProductos);
   fetch ("productos.json")
-  .then ((Response) => {
-    console.log (Response);
-  return Response.json ();
+  .then ((response) => {
+    console.log (response);
+  return response.json ();
   })
-  
+  .then ((responseProductos) => {
+    renderizarProductos (responseProductos);
+  });
+
